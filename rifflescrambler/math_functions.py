94473b52219bf97497de5bf32918ae1b4f596218
Konrad Swierczynski <konrad.swierczynski@nokia.com>
2018-09-20 07:46:58 +0200

    Correct typo in riffle_perm, stub of trace trajectories

--- rifflescrambler/math_functions.py
+++ rifflescrambler/math_functions.py
@@ -6,8 +6,19 @@ def rank(b, i):
 def riffle_permutation(B):
     b = bin(B)[2:]
     b = '0' * (8 - len(b)) + b if len(b) < 8 else b[-8:]
-    riffle_permutation_func = lambda i: rank(b, i) if b[i] == '0' else rank(b, i) + b.count('1')
+    riffle_permutation_func = lambda i: rank(b, i) if b[i] == '0' else rank(b, i) + b.count('0')
     return [riffle_permutation_func(i) for i in range(8)]
+
+
+def inverted_riffle_permutation(B, searched_value):
+    perm = riffle_permutation(B)
+    return inverted_perm(perm, searched_value)
+
+
+def inverted_perm(perm, searched_value):
+    for index, value in enumerate(perm):
+        if value == searched_value:
+            return index
 
 
 def value_zero_in_lower_triangular(M):
@@ -46,7 +57,8 @@ def riffle_shuffle(n, seed, hash_func):
 def trace_trajectories(B):
     trajectories = [B[0]]
     for i in range(1, len(B)):
-        pass
-        # trajectoreis.append(inverted_riffle_permutation(B[i - 1], B[i]))
+        print(i, B[i-1], B[i], inverted_riffle_permutation(B[i - 1], B[i]), sep=" : ")
+        trajectories.append(inverted_riffle_permutation(B[i - 1], B[i]))
+    return trajectories
 
 riffle_shuffle(8, 2, lambda x, y: (x + y) % 2)
