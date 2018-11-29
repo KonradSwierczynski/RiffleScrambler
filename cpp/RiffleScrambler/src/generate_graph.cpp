35fb0021b1033fb18a1aacfa7f71191035e4c220
Konrad Swierczynski <konrad.swierczynski@nokia.com>
2018-11-29 13:48:19 +0100

    Initialise vetor in gen_graph with size

--- cpp/RiffleScrambler/src/generate_graph.cpp
+++ cpp/RiffleScrambler/src/generate_graph.cpp
@@ -12,17 +12,17 @@ riffle_graph gen_graph(const std::vector<uint64_t> seeds, const uint64_t g) {
     std::cout << "\tGenerating graph...";
     BinMatrix bin_matrix(seeds);
     const uint64_t length = 1 << g;
-    riffle_graph edges (2 * g, std::vector<std::vector<uint64_t> >(length, std::vector<uint64_t>()));
+    riffle_graph edges (2 * g, std::vector<std::vector<uint64_t>>(length, std::vector<uint64_t>(2)));
     for(uint64_t row = 0; row < g; row++) {
         auto rp = riffle_permutation(bin_matrix, row, length);
         for(uint64_t i = 0; i < length; i++) {
-            edges[row][rp[i]].push_back(i);
-            edges[2 * g - row - 1][i].push_back(rp[i]);
+            edges[row][rp[i]][0] = i;
+            edges[2 * g - row - 1][i][0] = rp[i];
         }
         auto crp = complement_riffle_permutation(bin_matrix, row, length);
         for(uint64_t i = 0; i < length; i++) {
-            edges[row][crp[i]].push_back(i);
-            edges[2 * g - row - 1][i].push_back(crp[i]);
+            edges[row][crp[i]][1] = i;
+            edges[2 * g - row - 1][i][1] = crp[i];
         }
     }
     std::cout << " done!" << std::endl;
